15b9f3e83c564309c3c4fce951ebd8445f3d697e
huzhouyu <a1085052074@qq.com>
2021-06-16 16:52:14 +0800

    .

--- lock/lock.go
+++ lock/lock.go
@@ -3,7 +3,7 @@ package lock
 import "sync"
 
 type RWMutex struct {
-	sync.RWMutex
+	rw   sync.RWMutex
 	lock sync.Mutex
 }
 
@@ -13,13 +13,40 @@ type RWMutex struct {
 func (rw *RWMutex) Lock() {
 	rw.lock.Lock()
 	defer rw.lock.Unlock()
-	rw.RWMutex.Lock()
+	rw.rw.Lock()
+}
+
+// Unlock unlocks rw for writing. It is a run-time error if rw is
+// not locked for writing on entry to Unlock.
+//
+// As with Mutexes, a locked RWMutex is not associated with a particular
+// goroutine. One goroutine may RLock (Lock) a RWMutex and then
+// arrange for another goroutine to RUnlock (Unlock) it.
+func (rw *RWMutex) Unlock() {
+	rw.rw.Unlock()
 }
 
 // UnLockAndRLock 释放写锁并获取读锁
 func (rw *RWMutex) UnLockAndRLock() {
 	rw.lock.Lock()
 	defer rw.lock.Unlock()
-	rw.Unlock()
-	rw.RLock()
+	rw.rw.Unlock()
+	rw.rw.RLock()
+}
+
+// RLock locks rw for reading.
+//
+// It should not be used for recursive read locking; a blocked Lock
+// call excludes new readers from acquiring the lock. See the
+// documentation on the RWMutex type.
+func (rw *RWMutex) RLock() {
+	rw.rw.RLock()
+}
+
+// RUnlock undoes a single RLock call;
+// it does not affect other simultaneous readers.
+// It is a run-time error if rw is not locked for reading
+// on entry to RUnlock.
+func (rw *RWMutex) RUnlock() {
+	rw.rw.RUnlock()
 }
